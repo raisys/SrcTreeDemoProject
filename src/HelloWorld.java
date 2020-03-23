4d1024e3a500392ad8971f233245e74d5af0a264
vikash.k.rai <vikash.k.rai@pwc.com>
2020-03-23 06:27:54 -0500

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+	
+	public static void printHello(String strPlace) {
+        System.out.println("Hello, "+ strPlace);
+    }
+
+}
