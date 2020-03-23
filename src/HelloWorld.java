c20d2314f084b8f3051072250fa37ed95f763729
Vikash <vikashraisit@gmail.com>
2020-03-23 06:50:33 -0500

    base copy

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,16 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-	System.out.println("Hello, IA-101");
-	    
-	System.out.println("Hello, IL-501");
     }
 	
 	public static void printHello(String strPlace) {
-        System.out.println("Hello, "+ strPlace);
-    }
-
-public static void printHelloIA(String strPlace) {
         System.out.println("Hello, "+ strPlace);
     }
 
